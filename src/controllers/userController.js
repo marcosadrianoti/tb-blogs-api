6b0481afcef7deaa07e0cc0a8e38adc1ac501ef4
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-05-11 20:26:24 -0300

    Req 04 done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,8 +5,12 @@ const createUser = async (req, res) => {
     const userData = req.body;
 
     const user = await UserService.createUser(userData);
+    
+    if (user.status !== 201) {
+      console.log('user------Entrou-------', user.message);
+      return res.status(user.status).json(user.message);
+    }
 
-    console.log('user', user);
     const { password: _password, ...userWithoutPassword } = userData;
 
     const token = createToken(userWithoutPassword);
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,20 +1,39 @@
-// const bcrypt = require('bcryptjs');
 const { User } = require('../models');
 
-const createUser = ({ displayName, email, password, image }) => {
-  const userCreated = User.create({ displayName, email, password, image });
-  return { status: 201, message: userCreated };
+const DISPLAY_NAME_MSG = { message: '"displayName" length must be at least 8 characters long' };
+
+const isEmailValid = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 };
-  // const salt = bcrypt.genSaltSync(5);
-  // const encryptedPassword = bcrypt.hashSync(password, salt);
-//   return User.create({ displayName, email, password, image});
-// }
 
-// const getUsers = () => User.findAll();
-
-// const getByUsername = (username) => User.findOne({ where: { username } });
+const getByEmail = (email) => User.findOne({ where: { email } });
 
 const getForLogin = (email, password) => User.findOne({ where: { email, password } });
+
+const createUser = async ({ displayName, email, password, image }) => {
+  if (displayName.length < 8) {
+    return { status: 400, message: DISPLAY_NAME_MSG };
+  }
+  if (!isEmailValid(email)) {
+    return { status: 400, message: { message: '"email" must be a valid email' } };
+  }
+  if (password.length < 6) {
+    return {
+      status: 400, message: { message: '"password" length must be at least 6 characters long' },
+    };
+  }
+  const registeredUser = await getByEmail(email);
+  if (registeredUser) {
+ return { status: 409, message: { message: 'User already registered' },
+    };
+  }
+  
+  const userCreated = await User.create({ displayName, email, password, image });
+  return { status: 201, message: userCreated };
+};
+
+// const getUsers = () => User.findAll();
 
 // const getByUserId = (userId) => User.findByPk(userId);
 
